08032e0fba5d5e4186c3543c6442bc695fd6b0d3
rubyRain0 <den015326@gmail.com>
2024-04-28 17:20:14 +0300

    v1.0, main task,unit testing,javadocs

---
+++ docs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"com.example","l":"IntLinkedList"},{"p":"com.example","l":"IntLinkedListTests"},{"p":"com.example","l":"Main"}]
